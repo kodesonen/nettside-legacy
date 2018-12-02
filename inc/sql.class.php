4985a28aef4cc0e33e7d73ae26bed942b8a57e5b
Sirajuddin Asjad <sirasjad@gmail.com>
2018-12-02 02:01:46 +0100

    Fiksa core klassen

--- inc/sql.class.php
+++ inc/sql.class.php
@@ -1,13 +1,14 @@
 <?php
 
 class sqlCommunication{
-	private $dbHost, $dbUser, $dbPass, $dbName;
+	private $config, $dbHost, $dbName, $dbUser, $dbPass;
+
 	function __construct(){
-		/*
-		$this->dbHost = $config['host'];
-		$this->dbName = $config['name'];
-		$this->dbUser = $config['user'];
-		$this->dbPass = $config['pass'];	
+		$this->config = parse_ini_file('config/sql_config.ini');
+		$this->dbHost = $this->config['hostname'];
+		$this->dbName = $this->config['database'];
+		$this->dbUser = $this->config['username'];
+		$this->dbPass = $this->config['password'];	
 		
 		try{
 			$pdo = new PDO("mysql:host=$this->dbHost;dbname=$this->dbName", $this->dbUser, $this->dbPass);
@@ -17,7 +18,6 @@ class sqlCommunication{
 			echo "<strong>MySQL Error:</strong> " . $e->getMessage();
 			die();
 		}
-		*/
 	}
 
 	public function sqlSelect(){
