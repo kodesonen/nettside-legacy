68bda7211a5ec29271ef5320e68207707e33dad5
Sirajuddin Asjad <sirasjad@gmail.com>
2019-07-19 20:20:48 +0200

    Fix

--- index.php
+++ index.php
@@ -16,7 +16,6 @@ $core = new Kodesonen;
 $core->validPage();
 $core->checkSession();
 $core->betaAccess();
-//GetTimezone();
 
 switch($_GET['side']){
     case 'hjem': include("pages/home.php"); break;
@@ -28,6 +27,7 @@ switch($_GET['side']){
 	case 'medlem': include("pages/medlem.php"); break;
     case 'admin': include("pages/admin.php"); break;
     case 'login': include("pages/login.php"); break;
+    case 'logout': $core->logout(); break;
     case 'endre-medlemmer': include("pages/endre-medlemmer.php"); break;
     case 'kursbehandler': include("pages/kursbehandler.php"); break;
     case 'kapittelbehandler': include("pages/kapittelbehandler.php"); break;
@@ -40,6 +40,7 @@ switch($_GET['side']){
     case 'skjul-medlem': include("pages/skjul-medlem.php"); break;
     case 'ny-utfordring': include("pages/ny-utfordring.php"); break;
     case 'endre-utfordring': include("pages/endre-utfordring.php"); break;
+    case 'endre-bruker': include("pages/endre-bruker.php"); break;
     case 'vilkar-og-personvern': include("pages/vilkar-og-personvern.php"); break;
     case '404': include("pages/404.php"); break;
     default: include("pages/home.php"); break;
