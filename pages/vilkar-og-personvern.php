c8d9706f88f7a8abfa1a9dca425b4adcb2b44959
Daniel Skryseth <danost@outlook.com>
2019-09-21 06:19:09 +0200

    Update vilkar-og-personvern.php

--- pages/vilkar-og-personvern.php
+++ pages/vilkar-og-personvern.php
@@ -7,17 +7,23 @@
 		<div class="text-info"><h1>Vilkår og personvern</h1></div>
 		<p>Ved å opprette et medlemskap i Kodesonen samtykker du til at dine personopplysninger kan både hentes inn og behandles i våre systemer. 
 		Dette samtykket varer helt til medlemskapet blir sagt opp. For å si opp sitt medlemskap i Kodesonen, kan enn kontakte Kodesonen 
-		på <a href="mailto:kontakt@kodesonen.no" class="hyperlink">kontakt@kodesonen.no</a>. Etter at medlemskapet har blitt sagt opp vil all din data bli slettet fra våre systemer.</p>
-		<br/><p>Som medlem i Kodesonen godtar du at Kodesonen kan bruke dine dataer til å registrere antall medlemmer, både som en total helhet og over studieretning. Slik at vi kan tilpasse vårt innhold i henhold til
-		utfordringer i sammenheng med events, web challenges og meetups på campus. I tillegg til dette, gir du Kodesonen din tillatelse til at Kodesonen kan kontakte deg på e-mail angående dette.
-		Det kan også påpekes at dine dataer ikke blir brukt med informasjonskapsler med mindre du er registrert som en forfatter og/eller styremedlem. Hvis du ønsker å skjule ditt medlemskap 
-		kan du benytte deg av vårt <a href="/?side=skjul-medlem" class="hyperlink">digitale skjema</a> eller kontakte oss på E-mail. Ved skjult medlemskap vil du beholde alle fordeler hos Kodesonen. Dine dataer vil fortsatt
+		på <a href="mailto:kontakt@kodesonen.no" class="hyperlink">kontakt@kodesonen.no</a>. Etter at medlemskapet har blitt sagt opp vil 
+		all din data bli slettet fra våre systemer.</p>
+		
+		<br/><p>Som medlem i Kodesonen godtar du at Kodesonen kan bruke dine dataer til å registrere antall medlemmer, både som en total helhet 
+		og over studieretning. Slik at vi kan tilpasse vårt innhold i henhold til utfordringer i sammenheng med events, web challenges og meetups 
+		på campus. I tillegg til dette, gir du Kodesonen din tillatelse til at Kodesonen kan kontakte deg på e-mail angående dette.
+		Det kan også påpekes at dine dataer ikke blir brukt med informasjonskapsler med mindre du er registrert som en forfatter og/eller styremedlem. 
+		Hvis du ønsker å skjule ditt medlemskap kan du benytte deg av vårt <a href="/?side=skjul-medlem" class="hyperlink">digitale skjema</a> 
+		eller kontakte oss på E-mail. Ved skjult medlemskap vil du beholde alle fordeler hos Kodesonen. Dine dataer vil fortsatt
 		være lagret i våre systemer, men bli skjult fra den offentlige <a href="/?side=medlemsliste" class="hyperlink">medlemslisten.</a></p>
 		<br/><p>Er du registrert som forfatter og/eller styremedlem vil du ha enten full eller begrenset tilgang til kontrollpanelet på Kodesonen sitt nettsted. 
-		Her vil du kunne gjøre endringer som kan påvirke nettstedet <a href="/" class="hyperlink">Kodesonen.no</a>. Du må derfor godta forholdsregler som forfatter og/eller styremedlem i Kodesonen. 
-		Dette inkluderer det å ikke bryte opphavsrett når nytt innhold blir enten publisert eller gjengitt. Du kan ikke publisere sensitive personopplysninger, krenkende bilder eller tekst, spam eller reklamering av produkter og tjenester, sjikane eller trusler.
-		Om dette blir brutt vil du umiddelbart bli utestengt fra Kodesonen. Alle handlinger som blir gjort via kontrollpanelet vil bli loggført på din Kodesonen bruker.
-		Som forfatter godtar du også at ditt fulle navn blir brukt på hvert innlegg som blir skrevet under <a href="/?side=kurskatalog" class="hyperlink">kurskatalogen</a> gjennom kontrollpanelet.</p>
+		Her vil du kunne gjøre endringer som kan påvirke nettstedet <a href="/" class="hyperlink">Kodesonen.no</a>. Du må derfor godta 
+		forholdsregler som forfatter og/eller styremedlem i Kodesonen. Dette inkluderer det å ikke bryte opphavsrett når nytt innhold blir 
+		enten publisert eller gjengitt. Du kan ikke publisere sensitive personopplysninger, krenkende bilder eller tekst, spam eller 
+		reklamering av produkter og tjenester, sjikane eller trusler. Om dette blir brutt vil du umiddelbart bli utestengt fra Kodesonen. 
+		Alle handlinger som blir gjort via kontrollpanelet vil bli loggført på din Kodesonen bruker. Som forfatter godtar du også at ditt fulle navn 
+		blir brukt på hvert innlegg som blir skrevet under <a href="/?side=kurskatalog" class="hyperlink">kurskatalogen</a> gjennom kontrollpanelet.</p>
 		<br/><p>Kodesonen vil aldri spørre om og/eller bruke sensitive personopplysninger om deg.</p>
 	</div>
 </div>
